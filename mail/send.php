9f534a7b8e4efadbc641767e42a92fd6faa71313
sami-ou <78302181+sami-ou@users.noreply.github.com>
2021-07-08 16:44:36 +0100

    Update send.php

--- mail/send.php
+++ mail/send.php
@@ -6,18 +6,6 @@ require '/PHPMailer/Exception.php';
 require '/PHPMailer/PHPMailer.php';
 require '/PHPMailer/SMTP.php';
 
-$name=$_POST['name'];
-$email=$_POST['e-direction'];
-$phone=$_POST['phone'];
-$message=$_POST['message'];
-
-$dest="samioulkadi@gmail.com";
-$subject="contacto desde rochaworld.com";
-
-$carta="De: $name \n";
-$carta .="Correo: $email \n";
-$carta .="Numero de telefono: $phone \n";
-$carta .="Mensaje: $message \n";
 
 
 $mail = new PHPMailer(true);
@@ -28,25 +16,25 @@ try {
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-    $mail->Username   = 'samioulkadi@gmail.com';                     //SMTP username
+    $mail->Username   = 'rochawordlsarl@gmail.com';                     //SMTP username
     $mail->Password   = 'nilofer999';                               //SMTP password
     $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
     $mail->Port       = 465;                                    //TCP port to connect to; use 587 if you have set `SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS`
 
     //Recipients
-    $mail->setFrom('samioulkadi@gmail.com', 'sami');
-    $mail->addAddress('samioulkadi@gmail.com');     //Add a recipient
+    $mail->setFrom('rochawordlsarl@gmail.com', 'sami');
+    $mail->addAddress('rochawordlsarl@gmail.com');     //Add a recipient
     
     
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = $subject;
-    $mail->Body    = $carta;
+    $mail->Subject = 'Here is the subject';
+    $mail->Body    = 'This is the HTML message body <b>in bold!</b>';
     $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 
     $mail->send();
-    header('Location:sent-message.html');
+    echo 'Message has been sent';
 } catch (Exception $e) {
     echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
